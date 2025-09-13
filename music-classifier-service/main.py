680c658b7d60ff7eec7cec6e408321c4797c466d
Dinushka-Aththanayake <161476349+Dinushka-Aththanayake@users.noreply.github.com>
2025-09-13 12:59:59 +0530

    created getmood endpoint

--- music-classifier-service/main.py
+++ music-classifier-service/main.py
@@ -1,11 +1,14 @@
 from fastapi import FastAPI
 import uvicorn
+from predict_mood import predict_mood_from_mp3, mood_index_to_label
 
 app = FastAPI()
 
-@app.get("/hii")
-def read_root():
-    return "Hello, World!"
+@app.get("/getmood")
+def read_root(file_path: str):
+    mood = predict_mood_from_mp3(file_path)
+    mood = mood_index_to_label(mood)
+    return {"mood": mood}
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="0.0.0.0", port=8001, log_level="info")
