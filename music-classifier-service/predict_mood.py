221b28bbc49c3d84c151be6957bffcf09d25b5b6
Dinushka-Aththanayake <161476349+Dinushka-Aththanayake@users.noreply.github.com>
2025-09-13 12:58:26 +0530

    Revert "getfeature end point created"
    This reverts commit 1ed865eb98fd5a99c3f315d366a1730ee470e7ac.

--- music-classifier-service/predict_mood.py
+++ music-classifier-service/predict_mood.py
@@ -12,6 +12,22 @@ scaler = joblib.load(r"D:\Projects\Sonex\music-classifier-service\music-classifi
 encoder = joblib.load(r"D:\Projects\Sonex\music-classifier-service\music-classifier-service\assets\label_encoder.pkl")
 
 
+# === Predict mood ===
+def predict_mood_from_mp3(file_path):
+    mfcc = extract_mfcc_from_mp3(file_path)
+    if mfcc is None:
+        return "Error extracting features"
+
+    # Normalize
+    mfcc_scaled = scaler.transform(mfcc)
+    mfcc_scaled = mfcc_scaled.reshape(1, 130, 20)  # Shape expected by model
+
+    # Predict
+    predictions = model.predict(mfcc_scaled)
+    predicted_index = np.argmax(predictions)
+    predicted_mood = encoder.inverse_transform([predicted_index])[0]
+    return predicted_mood
+
 # === Define feature extraction ===
 def extract_mfcc_from_mp3(file_path, max_len=130, n_mfcc=20):
     try:
@@ -30,22 +46,6 @@ def extract_mfcc_from_mp3(file_path, max_len=130, n_mfcc=20):
         print("Failed to process file:", e)
         return None
 
-# === Predict mood ===
-def predict_mood_from_mp3(file_path):
-    mfcc = extract_mfcc_from_mp3(file_path)
-    if mfcc is None:
-        return "Error extracting features"
-
-    # Normalize
-    mfcc_scaled = scaler.transform(mfcc)
-    mfcc_scaled = mfcc_scaled.reshape(1, 130, 20)  # Shape expected by model
-
-    # Predict
-    predictions = model.predict(mfcc_scaled)
-    predicted_index = np.argmax(predictions)
-    predicted_mood = encoder.inverse_transform([predicted_index])[0]
-    return predicted_mood
-
 def mood_index_to_label(index):
     mood_labels = {
         0: "happy",
@@ -57,5 +57,6 @@ def mood_index_to_label(index):
 
 file_path = r"D:\\Projects\\Sonex\\music-classifier-service\\music-classifier-service\\sampletracks\\sample.mp3"
 
-# mood = predict_mood_from_mp3(file_path)
-# mood = mood_index_to_label(mood)
+mood = predict_mood_from_mp3(file_path)
+mood = mood_index_to_label(mood)
+print("Predicted Mood:", mood)
